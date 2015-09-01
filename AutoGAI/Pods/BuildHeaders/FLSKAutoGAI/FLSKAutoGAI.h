1c07897bb02ed3445db3426faf11b8d2b3cb075d
karlo <karlo@tonsser.com>
2015-09-01 12:56:59 +0200

    bumping version:

--- AutoGAI/Pods/BuildHeaders/FLSKAutoGAI/FLSKAutoGAI.h
+++
@@ -1,1 +0,0 @@
-../../../../Pod/Classes/FLSKAutoGAI.h
---
+++ AutoGAI/Pods/Target Support Files/FLSKAutoGAI/FLSKAutoGAI-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_FLSKAutoGAI : NSObject
+@end
+@implementation PodsDummy_FLSKAutoGAI
+@end
